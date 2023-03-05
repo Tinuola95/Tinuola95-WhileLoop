da17cf368a8e8b26374d1d4fa2e2c3876bbf5f72
Tinuola Ogunniki <ogunnoikiace@yahoo.com>
2023-03-05 19:53:13 +0000

    COmpleted

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -37,7 +37,15 @@ public class Lab {
         int i = 0;
 
 //        code here
+while (i<end){
+    System.out.println(sum);
+    System.out.println(i);
+    sum = sum + i;
+    i = i + 1;
+    
+    sum++;
+}
 
-        return sum;
+    return sum - end;
     }
 }
